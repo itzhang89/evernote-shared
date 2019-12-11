066a0093ab097d90a76efad4c5a0873ef6ac196b
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2019-12-11 17:10:21 +0800

    RabbitMQ 怎么告知生产者消息已经到达队列呢？ 1. 客户端增加回调方法 addReturnListener() 2. 客户端消息发送设置 mandatory=true(会在Exchange无法根据自身的路由键找到一个符合的队列，那么 RabbitMQ 会调用 Basic.Return 命令将消息返回给生产者)

--- others/src/main/java/example/rabbitmq/gitchat01/MqManager.java
+++ others/src/main/java/example/rabbitmq/gitchat01/MqManager.java
@@ -1,5 +1,6 @@
 package example.rabbitmq.gitchat01;
 
+import com.rabbitmq.client.ConfirmListener;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
@@ -9,6 +10,19 @@ import java.util.concurrent.TimeoutException;
 public class MqManager {
   static final String TASK_QUEUE_NAME = "hello";
   static final String EXCHANGE_NAME = "logs";
+  static final ConfirmListener CONFIRM_LISTENER = new ConfirmListener() {
+
+    @Override
+    public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+      // multiple：测试发现multiple随机true或false，原因未知
+      System.out.println("Nack deliveryTag:" + deliveryTag + ",multiple:" + multiple);
+    }
+
+    @Override
+    public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+      System.out.println("Ack deliveryTag:" + deliveryTag + ",multiple:" + multiple);
+    }
+  };
 
   static Connection createConnection() throws IOException, TimeoutException {
     ConnectionFactory factory = new ConnectionFactory();
@@ -20,5 +34,4 @@ public class MqManager {
     // 创建连接
     return factory.newConnection();
   }
-
 }
