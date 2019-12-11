8b41f7080fb77aa630204bdf8d6e6250a7a41a34
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2019-12-11 17:30:11 +0800

    RabbitMQ 怎么确保消息被消费者消费呢？消费者接收消息后发送 Basic.Ack 给 RabbitMQ 进行消费确认。

--- others/src/main/java/example/rabbitmq/gitchat01/BasicReceiver.java
+++ others/src/main/java/example/rabbitmq/gitchat01/BasicReceiver.java
@@ -3,11 +3,12 @@ package example.rabbitmq.gitchat01;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
 import java.io.IOException;
+
+import static example.rabbitmq.gitchat01.MqManager.TASK_QUEUE_NAME;
 
 public class BasicReceiver {
 
@@ -18,13 +19,12 @@ public class BasicReceiver {
     System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
     // consumer 简单的将接收到的消息打印出来
-    Consumer consumer = new DefaultConsumer(channel) {
+    channel.basicConsume(TASK_QUEUE_NAME, true, new DefaultConsumer(channel) {
       @Override
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
         String message = new String(body, "UTF-8");
         System.out.println(" [x] Received '" + message + "'");
       }
-    };
-    channel.basicConsume(MqManager.TASK_QUEUE_NAME, true, consumer);
+    });
   }
 }
---
+++ others/src/main/java/example/rabbitmq/gitchat01/ConfirmConsumerReceiver.java
@@ -0,0 +1,32 @@
+package example.rabbitmq.gitchat01;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import static example.rabbitmq.gitchat01.MqManager.TASK_QUEUE_NAME;
+
+public class ConfirmConsumerReceiver {
+
+  public static void main(String[] args) throws IOException, TimeoutException {
+    Connection conn = MqManager.createConnection();
+    Channel channel = conn.createChannel();
+
+    // 消费消息
+    channel.basicConsume(TASK_QUEUE_NAME, false, "myConsumer Tag", new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body) throws IOException {
+        String routingKey = envelope.getRoutingKey();
+        String contentType = properties.getContentType();
+        long deliveryTag = envelope.getDeliveryTag();
+        System.out.println("routingKey:" + routingKey + ",contentType:" + contentType + ",deliveryTag:" + deliveryTag + ",Msg body:" + new String(body));
+        channel.basicAck(deliveryTag, false);
+      }
+    });
+  }
+}
